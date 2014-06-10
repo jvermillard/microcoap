0586f578f5e6226e08880d28e343359d5efc59b0
Julien Vermillard <jvermillard@sierrawireless.com>
2014-06-10 18:12:37 +0200

    LED strip

--- endpoints.c
+++ endpoints.c
@@ -1,19 +1,31 @@
+
 #include <stdbool.h>
 #include <string.h>
 #include "coap.h"
 
-static char light = '0';
+static int lightR1 = 0;
+static int lightG1 = 0;
+static int lightB1 = 0;
 
-const uint16_t rsplen = 1500;
-static char rsp[1500] = "";
+static int lightR2 = 0;
+static int lightG2 = 0;
+static int lightB2 = 0;
+
+const uint16_t rsplen = 2500;
+static char rsp[2500] = "";
 void build_rsp(void);
 
 #ifdef ARDUINO
+
 #include "Arduino.h"
-static int led = 6;
+
+
+//static int led = 6;
 void endpoint_setup(void)
 {                
-    pinMode(led, OUTPUT);     
+
+  
+  //  pinMode(led, OUTPUT);     
     build_rsp();
 }
 #else
@@ -27,47 +39,154 @@ void endpoint_setup(void)
 static const coap_endpoint_path_t path_well_known_core = {2, {".well-known", "core"}};
 static int handle_get_well_known_core(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
 {
-    return coap_make_response(scratch, outpkt, (const uint8_t *)rsp, strlen(rsp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_APPLICATION_LINKFORMAT);
+  return coap_make_response(scratch, outpkt, (const uint8_t *)rsp, strlen(rsp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_APPLICATION_LINKFORMAT);
 }
 
-static const coap_endpoint_path_t path_light = {1, {"light"}};
-static int handle_get_light(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+static const coap_endpoint_path_t path_lightR1 = {2, {"color1","R"}};
+static const coap_endpoint_path_t path_lightG1 = {2, {"color1","G"}};
+static const coap_endpoint_path_t path_lightB1 = {2, {"color1","B"}};
+
+static const coap_endpoint_path_t path_lightR2 = {2, {"color2","R"}};
+static const coap_endpoint_path_t path_lightG2 = {2, {"color2","G"}};
+static const coap_endpoint_path_t path_lightB2 = {2, {"color2","B"}};
+
+static int handle_get_lightR1(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
 {
-    return coap_make_response(scratch, outpkt, (const uint8_t *)&light, 1, id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+    char tmp[30];
+    sprintf(tmp,"%d",lightR1);
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
 }
 
-static int handle_put_light(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+static int handle_get_lightR2(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    char tmp[30];
+    sprintf(tmp,"%d",lightR2);
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_get_lightG1(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    char tmp[30];
+    sprintf(tmp,"%d",lightG1);
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_get_lightG2(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    char tmp[30];
+    sprintf(tmp,"%d",lightG2);
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_get_lightB1(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    char tmp[30];
+    sprintf(tmp,"%d",lightB1);
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_get_lightB2(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    char tmp[30];
+    sprintf(tmp,"%d",lightB2);
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_lightR1(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
 {
     if (inpkt->payload.len == 0)
         return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
-    if (inpkt->payload.p[0] == '1')
-    {
-        light = '1';
-#ifdef ARDUINO
-        digitalWrite(led, HIGH);
-#else
-        printf("ON\n");
-#endif
-        return coap_make_response(scratch, outpkt, (const uint8_t *)&light, 1, id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
-    }
-    else
-    {
-        light = '0';
-#ifdef ARDUINO
-        digitalWrite(led, LOW);
-#else
-        printf("OFF\n");
-#endif
-        return coap_make_response(scratch, outpkt, (const uint8_t *)&light, 1, id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
-    }
+    
+    lightR1 = atoi(inpkt->payload.p);
+    stripSet1(lightR1, lightG1, lightB1);
+    
+    char tmp[30];
+    sprintf(tmp,"%d",lightR1);
+    return coap_make_response(scratch, outpkt,  (const uint8_t *)tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_lightR2(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    if (inpkt->payload.len == 0)
+        return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
+    
+    lightR2 = atoi(inpkt->payload.p);
+    stripSet2(lightR2, lightG2, lightB2);
+    
+    char tmp[30];
+    sprintf(tmp,"%d",lightR2);
+    return coap_make_response(scratch, outpkt,  (const uint8_t *)tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_lightG1(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    if (inpkt->payload.len == 0)
+        return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
+    
+    lightG1 = atoi(inpkt->payload.p);
+    stripSet1(lightR1, lightG1, lightB1);
+     
+    char tmp[30];
+    sprintf(tmp,"%d",lightG1);
+    return coap_make_response(scratch, outpkt,  (const uint8_t *)tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_lightG2(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    if (inpkt->payload.len == 0)
+        return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
+    
+    lightG2 = atoi(inpkt->payload.p);
+    stripSet2(lightR2, lightG2, lightB2);
+     
+    char tmp[30];
+    sprintf(tmp,"%d",lightG2);
+    return coap_make_response(scratch, outpkt,  (const uint8_t *)tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_lightB1(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    if (inpkt->payload.len == 0)
+        return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
+    
+    lightB1 = atoi(inpkt->payload.p);
+    stripSet1(lightR1, lightG1, lightB1);
+     
+    char tmp[30];
+    sprintf(tmp,"%d",lightB1);
+    return coap_make_response(scratch, outpkt,  (const uint8_t *)tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_lightB2(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    if (inpkt->payload.len == 0)
+        return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
+    
+    lightB2 = atoi(inpkt->payload.p);
+    stripSet2(lightR2, lightG2, lightB2);
+     
+    char tmp[30];
+    sprintf(tmp,"%d",lightB2);
+    return coap_make_response(scratch, outpkt,  (const uint8_t *)tmp, strlen(tmp), id_hi, id_lo, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
 }
 
 const coap_endpoint_t endpoints[] =
 {
     {COAP_METHOD_GET, handle_get_well_known_core, &path_well_known_core, "ct=40"},
-    {COAP_METHOD_GET, handle_get_light, &path_light, "ct=0"},
-    {COAP_METHOD_PUT, handle_put_light, &path_light, NULL},
+    {COAP_METHOD_GET, handle_get_lightR1, &path_lightR1, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_lightR1, &path_lightR1, NULL},
+    {COAP_METHOD_GET, handle_get_lightG1, &path_lightG1, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_lightG1, &path_lightG1, NULL},
+    {COAP_METHOD_GET, handle_get_lightB1, &path_lightB1, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_lightB1, &path_lightB1, NULL},
+    {COAP_METHOD_GET, handle_get_lightR2, &path_lightR2, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_lightR2, &path_lightR2, NULL},
+    {COAP_METHOD_GET, handle_get_lightG2, &path_lightG2, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_lightG2, &path_lightG2, NULL},
+    {COAP_METHOD_GET, handle_get_lightB2, &path_lightB2, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_lightB2, &path_lightB2, NULL},
     {(coap_method_t)0, NULL, NULL, NULL}
+
 };
 
 void build_rsp(void)
--- microcoap.ino
+++ microcoap.ino
@@ -1,3 +1,5 @@
+#include <LPD8806.h>
+
 /*
 * WARNING - UDP_TX_PACKET_MAX_SIZE is hardcoded by Arduino to 24 bytes
 * This limits the size of possible outbound UDP packets
@@ -6,11 +8,15 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <stdint.h>
-#include <EthernetUdp.h>
+//#include <EthernetUdp.h>
 #include "coap.h"
 
 #define PORT 5683
-static uint8_t mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+static uint8_t mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x04};
+IPAddress ip(10, 41, 51, 38);
+IPAddress gateway(10, 41,  51, 254);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dnsaddy(8,8,8,8);
 
 EthernetClient client;
 EthernetUDP udp;
@@ -18,21 +24,59 @@ uint8_t packetbuf[256];
 static uint8_t scratch_raw[32];
 static coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
 
+
+// Number of RGB LEDs in strand:
+int nLEDs = 64;
+
+// Chose 2 pins for output; can be any valid output pins:
+int dataPin  = 2;
+int clockPin = 3;
+
+// First parameter is the number of LEDs in the strand.  The LED strips
+// are 32 LEDs per meter but you can extend or cut the strip.  Next two
+// parameters are SPI data and clock pins:
+LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
+
+
+int r1=0;
+int g1=0;
+int b1=0;
+
+int r2=0;
+int g2=0;
+int b2=0;
+
+extern "C" {
+
+  void stripSet1(int r, int g, int b) {
+    r1=r;
+    g1=g;
+    b1=b;
+  }
+
+  void stripSet2(int r, int g, int b) {
+    r2=r;
+    g2=g;
+    b2=b;
+  }
+}
+
 void setup()
 {
+
     int i;
     Serial.begin(9600);
     while (!Serial) 
     {
-        ; // wait for serial port to connect. Needed for Leonardo only
+      ; // wait for serial port to connect. Needed for Leonardo only
     }
-
+Serial.println("Hello CoAP");
+Serial.flush();
     // start the Ethernet connection:
-    if (Ethernet.begin(mac) == 0)
-    {
-        Serial.println("Failed to configure Ethernet using DHCP");
-        while(1);
-    }
+Serial.println("Configuring static IP address");   
+    
+    Ethernet.begin(mac, ip, dnsaddy, gateway, subnet);
+    
     Serial.print("My IP address: ");
     for (i=0;i<4;i++)
     {
@@ -42,6 +86,12 @@ void setup()
     Serial.println();
     udp.begin(PORT);
 
+
+  strip.begin();
+
+  // Update the strip, to start they are all 'off'
+  strip.show();
+  
     coap_setup();
     endpoint_setup();
 }
@@ -54,6 +104,8 @@ void udp_send(const uint8_t *buf, int buflen)
     udp.endPacket();
 }
 
+int it=0;
+int subit=0;
 void loop()
 {
     int sz;
@@ -95,5 +147,28 @@ void loop()
             }
         }
     }
+    
+    // update led strip
+    
+    strip.show();
+    subit++;
+    if (subit >5) {
+      subit=0;
+      it=(it+1) % nLEDs;      
+      float dr = r2 -r1 / (float) nLEDs;
+      float dg = g2 -g1 / (float) nLEDs;
+      float db = b2 -b1 / (float) nLEDs;
+      
+      for (i=0; i < strip.numPixels(); i++) {    
+        
+        int r = (i+it)* dr + r1;
+        int g = (i+it)* dg + g1;
+        int b = (i+it)* db + b1;
+        strip.setPixelColor(i, strip.Color(r,g,b));
+      }  
+    }
+    
+   // Serial.print("IT: ");
+   // Serial.println(it);
 }
 
